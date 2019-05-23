0329cd46dc15d443daeef5ba64e5a5e198877cd5
Patrick Owens <manbearpat@gmail.com>
2019-05-22 20:39:34 -0700

    updates

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,6 +10,16 @@ function getMovie() {
 
     axios.post("/get-movie", movie).then((res) => {
         console.log(res)
+
+        var newCard = `
+        <div class="card" style="width: 18rem;">
+        <img src="${res.data.Poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">${res.data.Title}</p>
+        </div>
+      </div>`;
+
+      $("#movie-card-holder").append(newCard);
     });
 
     
@@ -25,6 +35,16 @@ function getBands() {
 
     axios.post("/get-bands", band).then((res) => {
         console.log(res)
+
+        var newCard = `
+        <div class="card" style="width: 18rem;">
+        <img src="${res.data.Poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">${res.data.Title}</p>
+        </div>
+      </div>`;
+
+      $("#band-card-holder").append(newCard);
     })
 }
 
@@ -38,6 +58,17 @@ function getSong() {
 
     axios.post("/get-songs", song).then((res) => {
         console.log(res)
+
+
+        var newCard = `
+        <div class="card" style="width: 18rem;">
+        <img src="${res.data.Poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">${res.data.Title}</p>
+        </div>
+      </div>`;
+
+      $("#song-card-holder").append(newCard);
     })
 }
 
@@ -56,31 +87,6 @@ $("#sSearch").on('click', function () {
 $("#mSearch").on('click', function () {
     getMovie();
 
-    //console.log(omdbSearch.val);
-    var newCard = $("<div>");
-
-    newCard.attr("class", "card card-body");
-
-
-//   <div class="card" style="width: 18rem;">
-// <img src="..." class="card-img-top" alt="...">
-// <div class="card-body">
-//   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-// </div>
-///div>
-
-    
-    var cardTitle = $("<p>");
-    cardTitle.attr("id", "cardTitleText");
-    cardTitle.text =  $("#omdbSearch").val().trim();
-    //newCard.append(CardTitle);
-    $("#cardTitleText").append(newCard);
-
-    
-    $("#card-holder").append(newCard);
-
-    
-    
 
 })
 
